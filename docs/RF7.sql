0033239507427f1576426c5b3f285b9b3e0425ec
Sofia Abadia Bermeo <s.abadia@uniandes.edu.co>
2020-05-02 14:31:50 -0500

    rf7 por ahora

---
+++ docs/RF7.sql
@@ -0,0 +1,48 @@
+UPDATE OFERTA
+SET oferta.activa='0'
+WHERE OFERTA.ID= 205;
+
+select distinct oferta.id as ofertas, reserva.id as cambiar
+from oferta, reserva
+where(oferta.id in (select distinct oferta.id
+                    from oferta, reserva
+                    where (oferta.id!= reserva.idoferta 
+                          and oferta.disponible= 'Y'
+                          and reserva.id in( select reserva.id
+                                             from reserva, oferta
+                                             where (reserva.idoferta=oferta.id and oferta.activa='0')
+                                            )
+                          and oferta.fechainicio= reserva.fechallegada
+                          )   
+                     ) 
+       and reserva.id in ( select distinct reserva.id
+                                             from reserva, oferta
+                                             where (reserva.idoferta=oferta.id and oferta.activa='0')
+                          )
+       --and rownum<10
+        )
+        ;
+
+select oferta.id
+from oferta
+where disponible='Y'
+
+--update reserva
+--set reserva.idoferta= oferta.id, reserva.id= reserva.id
+--from oferta, reserva
+--where(oferta.id in (select distinct oferta.id
+--                    from oferta, reserva
+--                    where (oferta.id!= reserva.idoferta 
+--                          and oferta.fechainicio<= reserva.fechallegada and oferta.fechafin >= reserva.fechaida
+--                          and reserva.id in( select reserva.id
+--                                             from reserva, oferta
+--                                               where (reserva.idoferta=oferta.id and oferta.activa='0')
+--                                            )
+--                          ) 
+--                     ) 
+--       and reserva.id in ( select reserva.id
+--                                             from reserva, oferta
+--                                             where (reserva.idoferta=oferta.id and oferta.activa='0')
+--                          )
+--               )         
+                    
