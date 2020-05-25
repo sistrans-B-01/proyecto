1582a484811ab2e67907752736801244a80ae782
Sofia Abadia Bermeo <s.abadia@uniandes.edu.co>
2020-05-25 18:58:17 -0500

    cambio fechas rfc10

--- docs/RFC10.sql
+++ docs/RFC10.sql
@@ -4,7 +4,7 @@ create index fechaofeclien on reserva(fechallegada, idoferta, idcliente);
 select 'HABITACION' as ALOJAMIENTO, cliente.*, oferta.id as idOferta, count(reserva.id) as reservas
 from cliente, oferta, reserva, ofertahabitacion
 where oferta.id= reserva.idoferta and cliente.numeroidentificacion= reserva.idcliente 
-      and reserva.fechallegada BETWEEN '31-DIC-2019' and '20-ENE-2020' 
+      and reserva.fechallegada BETWEEN '01-ene-2019' and '20-ENE-2019' 
       and ofertahabitacion.idoferta= oferta.id
 group by oferta.id, cliente.numeroidentificacion, cliente.tipocliente, cliente.tipoidentificacion, cliente.nombre
 
@@ -13,7 +13,7 @@ UNION
 select 'VIVIENDA' as ALOJAMIENTO, cliente.*, oferta.id as idOferta, count(reserva.id) as reservas
 from cliente, oferta, reserva, ofertavivienda
 where oferta.id= reserva.idoferta and cliente.numeroidentificacion= reserva.idcliente 
-      and reserva.fechallegada BETWEEN '31-DIC-2019' and '20-ENE-2020' 
+      and reserva.fechallegada BETWEEN '01-ene-2019' and '20-ENE-2019' 
       and ofertavivienda.idoferta= oferta.id
 group by oferta.id, cliente.numeroidentificacion, cliente.tipocliente, cliente.tipoidentificacion, cliente.nombre
 
@@ -22,7 +22,7 @@ UNION
 select 'APARTAMENTO' as ALOJAMIENTO, cliente.*, oferta.id as idOferta, count(reserva.id) as reservas
 from cliente, oferta, reserva, ofertaapartamento
 where oferta.id= reserva.idoferta and cliente.numeroidentificacion= reserva.idcliente 
-      and reserva.fechallegada BETWEEN '31-DIC-2019' and '20-ENE-2020' 
+      and reserva.fechallegada BETWEEN '19-ene-2019' and '20-ENE-2019' 
       and ofertaapartamento.idoferta= oferta.id
 group by oferta.id, cliente.numeroidentificacion, cliente.tipocliente, cliente.tipoidentificacion, cliente.nombre;
 
@@ -35,8 +35,8 @@ create index fechaofeclie on reserva(fechallegada, idcliente, idoferta);
 
 select 'HABITACION' as ALOJAMIENTO, cliente.*, oferta.id as idOferta, count(reserva.id) as reservas
 from cliente, oferta, reserva, ofertahabitacion
-where oferta.id= reserva.idoferta and cliente.numeroidentificacion= 305 and cliente.numeroidentificacion= reserva.idcliente 
-      and reserva.fechallegada BETWEEN '31-DIC-2019' and '20-ENE-2020' 
+where oferta.id= reserva.idoferta and cliente.numeroidentificacion= 94871 and cliente.numeroidentificacion= reserva.idcliente 
+      and reserva.fechallegada BETWEEN '01-ene-2019' and '20-ENE-2019' 
       and ofertahabitacion.idoferta= oferta.id
 group by oferta.id, cliente.numeroidentificacion, cliente.tipocliente, cliente.tipoidentificacion, cliente.nombre
 
@@ -44,8 +44,8 @@ UNION
 
 select 'VIVIENDA' as ALOJAMIENTO, cliente.*, oferta.id as idOferta, count(reserva.id) as reservas
 from cliente, oferta, reserva, ofertavivienda
-where oferta.id= reserva.idoferta and cliente.numeroidentificacion= 305 and cliente.numeroidentificacion= reserva.idcliente 
-      and reserva.fechallegada BETWEEN '31-DIC-2019' and '20-ENE-2020' 
+where oferta.id= reserva.idoferta and cliente.numeroidentificacion= 94871 and cliente.numeroidentificacion= reserva.idcliente 
+      and reserva.fechallegada BETWEEN '01-ene-2019' and '20-ENE-2019' 
       and ofertavivienda.idoferta= oferta.id
 group by oferta.id, cliente.numeroidentificacion, cliente.tipocliente, cliente.tipoidentificacion, cliente.nombre
 
@@ -53,8 +53,8 @@ UNION
 
 select 'APARTAMENTO' as ALOJAMIENTO, cliente.*, oferta.id as idOferta, count(reserva.id) as reservas
 from cliente, oferta, reserva, ofertaapartamento
-where oferta.id= reserva.idoferta and cliente.numeroidentificacion= 305 and cliente.numeroidentificacion= reserva.idcliente 
-      and reserva.fechallegada BETWEEN '31-DIC-2019' and '20-ENE-2020' 
+where oferta.id= reserva.idoferta and cliente.numeroidentificacion= 94871 and cliente.numeroidentificacion= reserva.idcliente 
+      and reserva.fechallegada BETWEEN '01-ene-2019' and '20-ENE-2019' 
       and ofertaapartamento.idoferta= oferta.id
 group by oferta.id, cliente.numeroidentificacion, cliente.tipocliente, cliente.tipoidentificacion, cliente.nombre;
 
